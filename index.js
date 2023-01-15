3233fae3ed6f0e528f756c37867cd3a54da8a5aa
quisethecoder <marquise_dawkins@aol.com>
2023-01-14 23:57:15 -0500

    fixing game

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-var randomNumber1 = Math.floor(Math.random() * 7);
+// var randomNumber1 = Math.floor(Math.random() * 7);
 
-var randomNumber2 = Math.floor(Math.random() * 7);
+// var randomNumber2 = Math.floor(Math.random() * 7);
 
-var randomDice = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+// var randomDice = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
 
-document.querySelector("img").setAttribute("src", randomDice[randomNumber1]);
+// document.querySelector("img").setAttribute("src", randomDice[randomNumber1]);
 
-document.querySelector("div").lastElementChild.lastElementChild.setAttribute("src", randomDice[randomNumber2]);
+// document.querySelector("div").lastElementChild.lastElementChild.setAttribute("src", randomDice[randomNumber2]);
 
 
-if (randomDice[randomNumber1] > randomDice[randomNumber2]){
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = ".images/dice" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+var randomImageSource2 = "images/dice" + randomNumber2 + "png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+
+
+if (randomNumber1 > randomNumber2){
 
     document.querySelector("h1").innerHTML= "Player 1 Wins! Refresh Page";
 
 
-} else if (randomDice[randomNumber1] === randomDice[randomNumber2]){
+} else if (randomNumber1 === randomNumber2){
 
     document.querySelector("h1").innerHTML= "It's a tie! Refresh Page";
 
