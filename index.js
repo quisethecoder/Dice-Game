4b6102be1630ff54dccddb8196efa0aaec5f1a82
quisethecoder <marquise_dawkins@aol.com>
2023-01-16 17:49:39 -0500

    dice image fix

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-// var randomNumber1 = Math.floor(Math.random() * 7);
-
-// var randomNumber2 = Math.floor(Math.random() * 7);
-
-// var randomDice = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
-
-// document.querySelector("img").setAttribute("src", randomDice[randomNumber1]);
-
-// document.querySelector("div").lastElementChild.lastElementChild.setAttribute("src", randomDice[randomNumber2]);
-
-
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
 var randomDiceImage = "dice" + randomNumber1 + ".png";
-var randomImageSource = ".images/dice" + randomDiceImage;
+
+var randomImageSource = "images/" + randomDiceImage;
+
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
-var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
-var randomImageSource2 = "images/dice" + randomNumber2 + "png";
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var imageSourcetwo = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", imageSourcetwo);
 
 
 
